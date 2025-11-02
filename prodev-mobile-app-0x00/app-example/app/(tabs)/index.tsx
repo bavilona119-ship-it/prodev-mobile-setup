48c77d3afacc38931699eb5e3a25b3003a4ae30b
bavilona119-ship-it <bavilona119@gmail.com>
2025-11-02 17:45:50 +0300

    Create index.tsx

---
+++ prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
@@ -0,0 +1,23 @@
+
+---
+
+## ✅ **app/(tabs)/index.tsx**
+
+```tsx
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text style={{ fontSize: 22, fontWeight: 'bold' }}>
+        ** First App Created**
+      </Text>
+    </View>
+  );
+}

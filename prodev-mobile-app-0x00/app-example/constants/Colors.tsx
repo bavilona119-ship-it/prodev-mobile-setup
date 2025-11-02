ea7dc30b1c6ad9d4957bc517e487b591c064c1f0
bavilona119-ship-it <bavilona119@gmail.com>
2025-11-02 17:48:59 +0300

    Create Colors.tsx

---
+++ prodev-mobile-app-0x00/app-example/constants/Colors.tsx
@@ -0,0 +1,16 @@
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: '#007aff',
+    tabIconDefault: '#ccc',
+    tabIconSelected: '#007aff',
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: '#0a84ff',
+    tabIconDefault: '#888',
+    tabIconSelected: '#0a84ff',
+  },
+};
